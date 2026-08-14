47a86438daa06b4a138ddb47b404af487af54745
Stefano Gallorini <himens@gmail.com>
2026-08-14 10:28:47 +0000

    Refactor BackpropNeuron class: enhance learning mechanism and update weight adjustment logic

--- examples/backprop/include/backprop-neuron.h
+++ examples/backprop/include/backprop-neuron.h
@@ -9,22 +9,40 @@ class BackpropNeuron : public hNNet::Neuron {
     public:
         BackpropNeuron() : hNNet::Neuron(std::make_unique<hNNet::SigmoidActivation>()) {}
     private:
-        // Update connection weight according to backprop learning rule
-        void update(SynapticConn *in_conn, const hNNet::real_t target) final {
-            const auto tx_signal = in_conn->tx->get_signal();
-            const auto derivative = this->get_activation().derivative(this->get_weighted_sum());
-            const auto out_connections = this->get_out_connections();
-            if (out_connections.empty()) {
-                const auto error = (target - this->get_signal());
-                _delta = error * derivative;
-            } 
-            else {
-                auto weighted_deltas = out_connections | std::views::transform([] (const auto &conn) { return conn->weight * static_cast<BackpropNeuron*>(conn->rx)->_delta; });
-                const auto weighted_delta = std::ranges::fold_left(weighted_deltas, 0.0, std::plus<>{});
-                _delta = weighted_delta * derivative;
+        // Learn from error
+        void learn(const hNNet::real_t error) final {
+            const auto derivative = this->get_activation().derivative(this->get_net_signal());
+            const auto delta = error * derivative;
+            for (const auto &conn : this->get_in_connections()) {
+                auto tx = dynamic_cast<BackpropNeuron*>(conn->tx);
+                if (tx == nullptr) {
+                    throw std::runtime_error("BackpropNeuron::learn: nullptr tx!");
+                }
+                const auto weighted_delta = delta * conn->weight;
+                tx->receive_delta(weighted_delta);
+                update(conn, delta); 
             }
-            in_conn->weight += tx_signal * _delta;
+        }
+        // Receive error from synaptic connection
+        void receive_delta(const hNNet::real_t delta) {
+            _rx_delta_sum += delta;
+            _number_rx_deltas++;
+            if (_number_rx_deltas == this->get_out_connections().size()) {
+                learn(_rx_delta_sum);
+                _number_rx_deltas = 0;
+                _rx_delta_sum = 0.0;
+            }
+        }
+        // Update synaptic weight
+        void update(SynapticConn *conn, const hNNet::real_t delta) {
+            if (conn->tx == nullptr) {
+                throw std::runtime_error("BackpropNeuron::update: nullptr tx!");
+            }
+            constexpr hNNet::real_t rate{0.2};
+            const auto tx_signal = conn->tx->get_signal();
+            conn->weight += rate * delta * tx_signal;
         }
         // Data members
-        hNNet::real_t _delta{0.0};
+        hNNet::size_t _number_rx_deltas{0};
+        hNNet::real_t _rx_delta_sum{0.0};
 };
